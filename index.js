3359c33f059b31807e6ca6d0a0113441a1d3620c
Bhargav M <bhargav.m@trutechlabs.in>
2025-07-01 15:50:39 +0530

    Update index.js
    trying to fix the port number

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+const PORT = process.env.PORT || 3000;
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Welcome to CLOUD ASEEM  & Credit with Thanks to - A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 80 , mention your port specific
+}).listen(PORT); //the server object listens on port 80 , mention your port specific
 
