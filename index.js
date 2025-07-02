32f9aa37c4c5feee6cbf4df1afeffc00e47495ef
Bhargav M <bhargav.m@trutechlabs.in>
2025-07-02 12:07:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.write('Welcome to CLOUD ASEEM  & Credit with Thanks to - A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
-}).listen(PORT); //the server object listens on port 80 , mention your port specific
+}).listen(PORT, () =>console.log('Server Started at PORT:${PORT} ')); //the server object listens on port 80 , mention your port specific
 
